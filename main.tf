f3bfa19c35bb1f1c92756b16ceeebf529ff8f90d
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-15 14:28:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_version = ">=0.14.5"
 
+  required_providers {
+    intersight = {
+      source  = "CiscoDevNet/intersight"
+      version = ">=1.0.18"
+    }
+  }
+}
+
+provider "intersight" {
+  apikey    = var.apikey
+  secretkey = var.secretkey
+  endpoint  = var.endpoint
+}
+
+data intersight_kubernetes_cluster iks_cluster {
+  
+
+}
