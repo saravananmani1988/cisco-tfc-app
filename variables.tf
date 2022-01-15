43545afc475c4ace7cec5327964dd22fa5984c51
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-15 15:29:14 +0530

    updated var

--- variables.tf
+++ variables.tf
@@ -28,3 +28,19 @@ variable "tags" {
   type    = list(map(string))
   default = []
 }
+variable "k8s_host" {
+  type        = string
+  description = "K8s cluster API server IP or host name"
+}
+variable "k8s_client_certificate" {
+  type        = string
+  description = "K8s client cert"
+}
+variable "k8s_client_key" {
+  type        = string
+  description = "k8s client cert key"
+}
+variable "k8s_cluster_ca_certificate" {
+  type        = string
+  description = "k8s ca server cert."
+}
