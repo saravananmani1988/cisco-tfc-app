28e7b44ddfb82229927d673add3e1d97c844a4ea
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-15 15:31:45 +0530

    updated k8s provider config

--- main.tf
+++ main.tf
@@ -9,4 +9,9 @@ terraform {
 
 provider "kubernetes" {
   # Configuration options
+  host = var.k8s_host
+
+  client_certificate     = var.k8s_client_certificate
+  client_key             = var.k8s_client_key
+  cluster_ca_certificate = var.k8s_cluster_ca_certificate
 }
