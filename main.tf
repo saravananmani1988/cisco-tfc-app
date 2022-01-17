dfef7cfee755f671a149c8dee64b4a9c63efaa09
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-17 21:18:01 +0530

    deploy db

--- main.tf
+++ main.tf
@@ -20,6 +20,106 @@ provider "kubernetes" {
 
 # deploy 2 tier app 
 
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "petclinic-db-mysql"
+    labels = {
+      test = "petclinic-db-mysql"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        test = "petclinic-db-mysql"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "petclinic-db-mysql"
+        }
+      }
+
+      spec {
+        container {
+          image = "mysql:5.7.33"
+          name  = "petclinic-db-mysql"
+		  env {
+            name = "MYSQL_ROOT_PASSWORD"
+            value = "supermysql"
+          }
+		  env {
+            name = "MYSQL_PASSWORD"
+            value = "supermysql"
+          }
+		  env {
+            name = "MYSQL_USER"
+            value = ""
+          }
+		  env{
+            name = "MYSQL_DATABASE"
+            value = "petclinic"
+          } 
+	  
+		  port {
+            container_port = 3306
+			name           = mysql
+			protocol	   = TCP
+          }
+		  
+		  liveness_probe {
+			exec  {
+			  command = "sh -c mysqladmin ping -u root -p${MYSQL_ROOT_PASSWORD}"
+			}
+			initial_delay_seconds = 30
+            period_seconds        = 10
+		    failure_threshold     = 3
+			success_threshold     = 1
+			timeout_seconds       =  5
+		  }
+		  
+		  readiness_probe {
+			exec  {
+			  command = "sh -c mysqladmin ping -u root -p${MYSQL_ROOT_PASSWORD}"
+			}
+			initial_delay_seconds = 3
+            period_seconds        = 10
+		    failure_threshold     = 5
+			success_threshold     = 1
+			timeout_seconds       =  1
+		  }
+		  
+		  resources {
+            limits = {
+              cpu    = "100m"
+              memory = "256Mi"
+            }
+
+           }
+		   
+		   volume {
+		   name = "data"
+		   persistent_volume_claim {
+		    claim_name = "petclinic-db-mysql"
+		   }
+		   
+		   }
+		   
+		   volume_mount {
+		   
+		   mount_path = "/var/lib/mysql"
+		   name       = "data"
+		   
+		   }
+      }
+    }
+  }
+}
+
 
 
 
